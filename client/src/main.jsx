3fe924ef6624243ac764fa55194f5637d10d376c
Marwan Zaarab <marwan.zaarab@gmail.com>
2023-11-06 16:05:39 -0600

    test

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,8 +5,6 @@ import { Provider } from 'mobx-react';
 import userStore from './stores/userStore.js';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import './main.css';
-import 'tw-elements-react/dist/css/tw-elements-react.min.css';
-import 'tw-elements/dist/css/tw-elements.min.css';
 
 const queryClient = new QueryClient();
 
