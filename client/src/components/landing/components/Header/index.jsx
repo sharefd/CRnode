72d8bdbfe5093dc1ccd142cec23f631d2693daf7
shar d <sharefdanho@gmail.com>
2023-12-24 16:08:24 -0500

    landing pictures changes

--- client/src/components/landing/components/Header/index.jsx
+++ client/src/components/landing/components/Header/index.jsx
@@ -3,17 +3,7 @@ import { Row, Col, Drawer } from 'antd';
 import Container from '../../common/Container';
 import { SvgIcon } from '../../common/SvgIcon';
 import { Button } from '../../common/Button';
-import {
-  HeaderSection,
-  LogoContainer,
-  Burger,
-  NotHidden,
-  Menu,
-  CustomNavLinkSmall,
-  Label,
-  Outline,
-  Span
-} from './styles';
+import { HeaderSection, LogoContainer, CustomNavLinkSmall, Label, Span } from './styles';
 import { useNavigate } from 'react-router';
 import Logo from '../Logo';
 
@@ -26,27 +16,27 @@ const Header = () => {
   };
 
   const MenuItem = () => {
-    const scrollTo = id => {
+    const scrollTo = (id) => {
       const element = document.getElementById(id);
       element.scrollIntoView({
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
       setVisibility(false);
     };
     return (
       <>
-        <CustomNavLinkSmall onClick={() => scrollTo('about')}>
+        {/* <CustomNavLinkSmall onClick={() => scrollTo('about')}>
           <Span>About</Span>
         </CustomNavLinkSmall>
-        <CustomNavLinkSmall onClick={() => scrollTo('mission')}>
-          <Span>Mission</Span>
-        </CustomNavLinkSmall>
+        <CustomNavLinkSmall onClick={() => scrollTo('mission')}> 
+          <Span>Mission</Span> 
+        </CustomNavLinkSmall>  
         <CustomNavLinkSmall onClick={() => scrollTo('product')}>
           <Span>Product</Span>
-        </CustomNavLinkSmall>
+        </CustomNavLinkSmall> */}
         <CustomNavLinkSmall style={{ width: '180px' }} onClick={() => navigate('/login')}>
           <Span>
-            <Button>Log In</Button>
+            <Button>Sign Up/Log In</Button>
           </Span>
         </CustomNavLinkSmall>
       </>
@@ -58,26 +48,11 @@ const Header = () => {
       <Container>
         <Row justify='space-between'>
           <Logo />
-          <NotHidden>
+          {/* <NotHidden>
             <MenuItem />
-          </NotHidden>
-          <Burger onClick={toggleButton}>
-            <Outline />
-          </Burger>
+          </NotHidden> */}
+          <MenuItem /> {/* Render the button directly for mobile devices */}
         </Row>
-        <Drawer closable={false} open={visible} onClose={toggleButton}>
-          <Col style={{ marginBottom: '2.5rem' }}>
-            <Label onClick={toggleButton}>
-              <Col span={12}>
-                <Menu>Menu</Menu>
-              </Col>
-              <Col span={12}>
-                <Outline />
-              </Col>
-            </Label>
-          </Col>
-          <MenuItem />
-        </Drawer>
       </Container>
     </HeaderSection>
   );
