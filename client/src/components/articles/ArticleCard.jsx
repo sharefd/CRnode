7f2aa1d1fb189f6907521ccaf253f4ff7cef32a1
shar d <sharefdanho@gmail.com>
2024-01-15 14:54:13 -0500

    new export cal button

--- client/src/components/articles/ArticleCard.jsx
+++ client/src/components/articles/ArticleCard.jsx
@@ -130,7 +130,7 @@ const ArticleCard = ({ article, isOrganizer, onFavorite, onEdit, isFavorite }) =
             </Col>
           )}
           <div className='absolute top-[-2px] right-0 mr-2'>
-            <ExportToIcalButton article={article} text='Export to iCal' fontSize='12px' />
+            <ExportToIcalButton article={article} text='Export' fontSize='12px' />
           </div>
 
           {!isVirtual && (
--- client/src/components/articles/calendar/ExportToIcalButton.jsx
+++ client/src/components/articles/calendar/ExportToIcalButton.jsx
@@ -1,10 +1,119 @@
-import { DownloadOutlined } from '@ant-design/icons';
+import { AppleOutlined, GoogleOutlined, CalendarOutlined } from '@ant-design/icons';
 import { createEvent } from 'ics';
 import { extractTimesFromDuration } from '@/utils/dates';
 import dayjs from 'dayjs';
+import { Dropdown, Menu } from 'antd';
+import React, { useState } from 'react';
 
-const ExportToIcalButton = ({ article, text, fontSize }) => {
-const createIcsFile = event => {
+
+
+const ExportButton = ({ article, text, fontSize }) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleMenuClick = ({ key }) => {
+    handleExport(article, key);
+    setVisible(false);
+  };
+
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="ical" icon={<AppleOutlined />}>
+        iCal
+      </Menu.Item>
+      <Menu.Item key="gmail" icon={<GoogleOutlined />}>
+        GCal
+      </Menu.Item>
+    </Menu>
+  );
+    
+  const createIcsFile = (event) => {
+    const {
+      title,
+      date,
+      duration,
+      location,
+      additional_details,
+      speaker,
+      organizer,
+      meeting_id,
+      passcode,
+      event_link,
+    } = event;
+    const [startTime, endTime] = extractTimesFromDuration(duration);
+
+    const startDate = dayjs(date).hour(startTime.hour()).minute(startTime.minute());
+    const endDate = dayjs(date).hour(endTime.hour()).minute(endTime.minute());
+
+    let description = '';
+
+    if (additional_details) {
+      description += `Details: ${additional_details}\n`;
+    }
+
+    if (speaker) {
+      description += `Speaker: ${speaker}\n`;
+    }
+
+    if (organizer) {
+      description += `Organized by: ${organizer.firstName} ${organizer.lastName}\n`;
+    }
+
+    if (meeting_id) {
+      description += `Meeting ID: ${meeting_id}\n`;
+    }
+
+    if (passcode) {
+      description += `Passcode: ${passcode}\n`;
+    }
+
+    const eventObject = {
+      title: title || 'Untitled Event',
+      start: [
+        startDate.year(),
+        startDate.month() + 1,
+        startDate.date(),
+        startDate.hour(),
+        startDate.minute(),
+      ],
+      end: [endDate.year(), endDate.month() + 1, endDate.date(), endDate.hour(), endDate.minute()],
+      location,
+    };
+
+    // Include url property only if event_link is not empty
+    if (event_link) {
+      eventObject.url = isValidUrl(event_link) ? event_link : '';
+    }
+
+    if (description) {
+      eventObject.description = description;
+    }
+
+    createEvent(eventObject, (error, value) => {
+      if (error) {
+        console.log(error);
+      } else {
+        const blob = new Blob([value], { type: 'text/calendar;charset=utf-8;' });
+        const link = document.createElement('a');
+        link.href = URL.createObjectURL(blob);
+        link.download = `${title.replace(/[^a-zA-Z ]/g, '') || 'untitled'}.ics`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      }
+    });
+  };
+
+  // URL validation function
+  const isValidUrl = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  };
+
+const createGmailLink = (event) => {
   const {
     title,
     date,
@@ -15,7 +124,7 @@ const createIcsFile = event => {
     organizer,
     meeting_id,
     passcode,
-    event_link
+    event_link,
   } = event;
   const [startTime, endTime] = extractTimesFromDuration(duration);
 
@@ -44,57 +153,45 @@ const createIcsFile = event => {
     description += `Passcode: ${passcode}\n`;
   }
 
-  const eventObject = {
-    title: title || 'Untitled Event',
-    start: [startDate.year(), startDate.month() + 1, startDate.date(), startDate.hour(), startDate.minute()],
-    end: [endDate.year(), endDate.month() + 1, endDate.date(), endDate.hour(), endDate.minute()],
-    location,
-  };
+  let gmailLink = `https://mail.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(
+    title || 'Untitled Event'
+  )}&dates=${startDate.format('YYYYMMDDTHHmmss')}%2F${endDate.format('YYYYMMDDTHHmmss')}&details=${encodeURIComponent(
+    description
+  )}&location=${encodeURIComponent(location)}`;
 
   // Include url property only if event_link is not empty
   if (event_link) {
-    eventObject.url = isValidUrl(event_link) ? event_link : '';
+    gmailLink += `&url=${isValidUrl(event_link) ? encodeURIComponent(event_link) : ''}`;
   }
 
-  if (description) {
-    eventObject.description = description;
-  }
+  return gmailLink;
+};
 
-  createEvent(eventObject, (error, value) => {
-    if (error) {
-      console.log(error);
-    } else {
-      const blob = new Blob([value], { type: 'text/calendar;charset=utf-8;' });
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(blob);
-      link.download = `${title.replace(/[^a-zA-Z ]/g, '') || 'untitled'}.ics`;
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
+
+  const openGmailLink = (event) => {
+    const gmailLink = createGmailLink(event);
+    window.open(gmailLink, '_blank');
+  };
+
+  const handleExport = (event, type) => {
+    if (type === 'ical') {
+      createIcsFile(event);
+    } else if (type === 'gmail') {
+      openGmailLink(event);
     }
-  });
-};
-
-// URL validation function
-const isValidUrl = (url) => {
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
-};
-
+  };
 
   return (
-    <div
-      onClick={() => createIcsFile(article)}
-      className='flex items-center cursor-pointer basic-btn red-full px-2 py-[3px] hover:bg-purple-100 rounded-md'>
-      <DownloadOutlined className='text-md text-[#f47d7f]' />
-      <p className='ml-1 text-[#f47d7f]' style={{ fontWeight: 700, fontFamily: 'sans-serif', fontSize: '12px' }}>
-        {text ? text : 'iCal'}
-      </p>
-    </div>
+    <Dropdown overlay={menu} placement="bottomRight" arrow>
+      <div className='flex items-center basic-btn red-full px-2 py-[3px] hover:bg-purple-100 rounded-md'>
+        <CalendarOutlined className='text-md text-[#f47d7f]' />
+        <p className='ml-1 text-[#f47d7f]' style={{ fontWeight: 700, fontFamily: 'sans-serif', fontSize: '12px' }}>
+          {text ? text : 'Export'}
+        </p>
+      </div>
+    </Dropdown>
   );
 };
-export default ExportToIcalButton;
+
+
+export default ExportButton;
